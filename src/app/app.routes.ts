725224fa49bb42b71c47972d7bdf9d4bae4944fa
Eugene Hermano <eunico.hermano@gmail.com>
2024-10-21 19:51:18 +0800

    With API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CandidateSearchComponent } from './pages/candidate-search/candidate-search.component';
+import { JobSearchComponent } from './pages/job-search/job-search.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'job-search', component: JobSearchComponent },
+
+  { path: 'candidate-search', component: CandidateSearchComponent },
+
+  { path: '', pathMatch: 'full', redirectTo: '/job-search' },
+];
---
+++ src/app/pages/candidate-search/candidate-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './candidate-search.component.html',
+  styleUrl: './candidate-search.component.scss'
+})
+export class CandidateSearchComponent {
+
+}
